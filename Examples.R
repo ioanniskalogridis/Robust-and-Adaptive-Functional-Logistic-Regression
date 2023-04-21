45662051dd5d976871cf2da14061aafaeaa4c56a
Ioannis Kalogridis <77626768+ioanniskalogridis@users.noreply.github.com>
2023-04-21 10:10:02 +0200

    Add files via upload

---
+++ Examples.R
@@ -0,0 +1,300 @@
+##################################################### Clean data #########################################################
+##########################################################################################################################
+
+nrep <- 1000
+n <- 400
+p <- 200
+eps <- 0
+grid <- seq(1/p, 1-1/p, len = p)
+
+ma <- m0 <- m05 <- m1 <- matrix(NA, nrow = p, ncol = nrep)
+
+msq = msq0 = msq05 = msq1 =  rep(NA, nrep)
+alpha.sel <- rep(NA, nrep)
+ic <- rep(NA, nrep)
+
+for(k in 498:nrep){
+  print(k)
+  # f1 <- 2*sin(40*grid*pi/3)
+  # f1 <- -sin(5*grid/1.2)/0.5-1
+  # f1 <- 3*sin(3.4*grid^2)
+  # f1 <- 2*cos(4*pi*grid)
+  # f1 <- 2*grid^2
+  f1 <- 3*(grid-0.3)^2+1
+  # f1 <- 2*grid/5 + 1
+  # f1 <- 1/(1+grid^2)
+  # f1 <- 1/(1+5*grid)
+  # x <- sqrt(2)*matrix(rnorm(n*p), nrow = n, ncol = p)
+  # X0 <- sqrt(2)*matrix(rt(n*p, df = 3), nrow = n, ncol = p)
+  x <- matrix(0, n, p)
+  for(i in 1:n){
+    x[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+    for(j in 2:50){
+      x[i, ] <- x[i, ] + (j*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  )
+    }
+  }
+  # par(mgp = c(3.5, 2.5, 0), mar = c(5.1, 5.1, 4.1, 2.1))
+  # matplot(grid,t(x), lwd = 3, col = "gray", type = "l", lty = 1, cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "")
+  # grid()
+  # pred.basis <- create.bspline.basis(c(0,1), nbasis = 13)
+  # pred.basis <- eval.basis(grid,pred.basis)
+  # C.m <- matrix(rnorm(n*13), nrow = n, ncol = 13)%*%matrix(runif(13*13), nrow = 13, ncol = 13)
+  # x <- t(pred.basis%*%t(C.m))  
+  # f1 <- sin(10*grid*pi/3)
+  
+  y0 = x%*%f1/p
+  # sum(abs(y0)>5)
+  # plot(inv.logit(y0))
+  
+  plot(y0,inv.logit(y0))
+  inv.logit <- function(x) 1/(1+exp(-x))
+  y <- rbinom(n, size = 1, prob = inv.logit(y0))
+  
+  fit <- dpd.f(x = x, y = y, norder = 4, m = 2)
+  # fit0 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 1e-20)
+  fit05 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 2.5)
+  fit1 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 5)
+  fit0 <- dpd.ffa(x, y)
+  msq[k] <- mean((f1-fit$est)^2)
+  alpha.sel[k] <- fit$alpha
+  ic[k] <- fit$ic
+  
+  msq0[k] <- mean((f1-fit0$est)^2)
+  msq05[k] <- mean((f1-fit05$est)^2)
+  msq1[k] <- mean((f1-fit1$est)^2)
+  
+  ma[, k] <- fit$est
+  m0[, k] <- fit0$est
+  m1[, k] <- fit1$est
+  m05[, k] <- fit05$est
+  
+}
+
+
+matplot(grid, m0, type = "l", col = "gray", lwd = 3)
+matplot(grid, ma, type = "l", col = "gray", lwd = 3); lines(grid, f1, lwd = 3, col = "black")
+
+m0.o <- m0[, order(msq0)]
+m0.o <- m0.o[, 1:700]
+
+ma.o <- ma[, order(msq)]
+ma.o <- ma.o[, 1:700]
+
+matplot(grid, m0.o, type = "l", col = "gray", lwd = 3, cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = ""); lines(grid, f1, lwd = 3, col = "black") ;grid()
+matplot(grid, ma.o, type = "l", col = "gray", lwd = 3,  cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = ""); lines(grid, f1, lwd = 3, col = "black") ; grid()
+
+hist(alpha.sel)
+# hist(ic)
+mean(msq, na.rm = TRUE)
+median(msq, na.rm = TRUE) ; median(msq0, na.rm = TRUE) ; median(msq05, na.rm = TRUE) ; median(msq1, na.rm = TRUE)
+median(alpha.sel, na.rm = TRUE)
+
+boot.median <- function(x){
+  b <- 10000
+  med.v <- rep(0, length(b))
+  for(i in 1:b){
+    r.samp <- sample(x, size = 1000, replace = TRUE)
+    med.v[i] <- median(r.samp)
+  }
+  sd.med <- sd(med.v)
+  print(sd.med)
+}
+
+boot.median(msq)
+boot.median(msq0)
+boot.median(msq05)
+boot.median(msq1)
+
+plot(grid, f1, type = "l", lwd = 3)
+lines(grid, fit$est, type = "l", lwd = 3, col = "blue")
+lines(grid, fit1$est, lwd = 3)
+lines(grid, fit0$est, lwd = 3, col = "red")
+
+###################################################### Contamination #######################################################
+############################################################################################################################
+
+nrep <- 1000
+n <- 400
+p <- 200
+eps <- 0.01
+grid <- seq(1/p, 1-1/p, len = p)
+
+msq = msq0 = msq05 = msq1 =  rep(NA, nrep)
+alpha.sel <- rep(NA, nrep)
+ic <- rep(NA, nrep)
+
+ma <- m0 <- m05 <- m1 <- matrix(NA, nrow = p, ncol = nrep)
+
+for(k in 1:nrep){
+  print(k)
+  # f1 <- 2*sin(40*grid*pi/3)
+  # f1 <- -sin(5*grid/1.2)/0.5-1
+  # f1 <- 3*exp(-grid^2)
+  # f1 <- 1/(1+5*grid)
+  # f1 <- 2*cos(4*grid*pi/3)
+  f1 <- 2*cos(grid*pi/4)
+  # f1 <- 3*sin(3.4*grid^2)
+  # f1 <- 2*cos(4*pi*grid)
+  # f1 <- 5*grid^2
+  # x <- sqrt(2)*matrix(rnorm(n*p), nrow = n, ncol = p)
+  # X0 <- sqrt(2)*matrix(rt(n*p, df = 3), nrow = n, ncol = p)
+  x <- matrix(0, n, p)
+  for(i in 1:n){
+    x[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+    for(j in 2:50){
+      x[i, ] <- x[i, ] + (j*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  )
+    }
+  }
+  samp <- sample(1:n, size = floor(n*eps))
+  # for(i in samp){
+  #   x[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rt(1, df = 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+  #   for(j in 2:10){
+  #     x[i, ] <- x[i, ] + (j*pi-pi/2)^{-1}*rt(1, df = 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  ) 
+  #   }
+  # }
+  x[samp, ] <- 5*x[samp, ]
+
+  y0 = (x%*%f1)/p
+  
+  # sum(abs(y0)>3)
+  # plot(inv.logit(y0))
+  
+  # plot(y0,inv.logit(y0))
+  inv.logit <- function(x) 1/(1+exp(-x))
+  y <- rbinom(n, size = 1, prob = inv.logit(y0))
+  y[samp]<- 1- y[samp]
+  
+  fit <- dpd.f(x = x, y = y, norder = 4, m = 2)
+  # fit0 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 1e-20)
+  fit05 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 2.5)
+  fit1 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 5)
+  fit0 <- p.ml(x, y, norder = 4, nbasis = 30, m = 2)
+  msq[k] <- mean((f1-fit$est)^2)
+  alpha.sel[k] <- fit$alpha
+  ic[k] <- fit$ic
+  
+  ma[, k] <- fit$est
+  m0[, k] <- fit0$est
+  m1[, k] <- fit1$est
+  m05[, k] <- fit05$est
+  
+  msq0[k] <- mean((f1-fit0$est)^2)
+  msq05[k] <- mean((f1-fit05$est)^2)
+  msq1[k] <- mean((f1-fit1$est)^2)
+}
+
+matplot(grid, m0, type = "l", col = "gray", lwd = 3)
+matplot(grid, ma, type = "l", col = "gray", lwd = 3); lines(grid, f1, lwd = 3, col = "black")
+
+hist(alpha.sel)
+# hist(ic)
+mean(msq, na.rm = TRUE)
+median(msq, na.rm = TRUE) ; median(msq0, na.rm = TRUE) ; median(msq05, na.rm = TRUE) ; median(msq1, na.rm = TRUE)
+median(alpha.sel, na.rm = TRUE)
+
+boot.median <- function(x){
+  b <- 10000
+  med.v <- rep(0, length(b))
+  for(i in 1:b){
+    r.samp <- sample(x, size = 1000, replace = TRUE)
+    med.v[i] <- median(r.samp)
+  }
+  sd.med <- sd(med.v)
+  print(sd.med)
+}
+
+boot.median(msq)
+boot.median(msq0)
+boot.median(msq05)
+boot.median(msq1)
+
+
+
+plot(grid, f1, type = "l", lwd = 3)
+lines(grid, fit$est, type = "l", lwd = 3, col = "blue")
+lines(grid, fit1$est, lwd = 3)
+lines(grid, fit0$est, lwd = 3, col = "red")
+
+#################################################################################################################################################################
+#################################################################################################################################################################
+
+nrep <- 30
+n <- 300
+p <- 250
+grid <- seq(1/p, 1-1/p, len = p)
+
+msq = msq0 = msq05 = msq1 =  rep(NA, nrep)
+alpha.sel <- rep(NA, nrep)
+ic <- rep(NA, nrep)
+
+for(k in 1:nrep){
+  print(k)
+  # f1 <- -sin(5*grid/1.2)/0.8-1
+  # f1 <- 1.8*sin(3.4*grid^2)
+  # f1 <- cos(2*pi*grid)
+  # f1 <- grid^2
+  # x <- sqrt(2)*matrix(rnorm(n*p), nrow = n, ncol = p)
+  # X0 <- sqrt(2)*matrix(rt(n*p, df = 3), nrow = n, ncol = p)
+  f1 <- sin(10*grid*pi/3)
+  f2 <- 2*sin(40*grid*grid/3)
+  # x1 <- matrix(0, floor(0.9*n), p)
+  pred.basis <- create.bspline.basis(c(0,1), nbasis = 13)
+  pred.basis <- eval.basis(grid,pred.basis)
+  C.m <- matrix(rnorm(n*13), nrow = n, ncol = 13)%*%matrix(runif(13*13), nrow = 13, ncol = 13)
+  x <- t(pred.basis%*%t(C.m))  
+  
+  # for(i in 1:floor(0.9*n)){
+  # x1[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+  # x[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rt(1, df = 3)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+  # for(j in 2:50){
+  # x1[i, ] <- x1[i, ] + (j*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  )
+  # x[i, ] <- x[i, ] + (j*pi-pi/2)^{-1}*rt(1, df = 3)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  ) # For leverage contamination
+  # }
+  # }
+  
+  l1 = x1%*%f1/p
+  
+  x2 <- matrix(0, (n -floor(0.9*n)), p)
+  for(i in 1:(n-floor(0.9*n))){
+    # x2[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+    x2[i, ] <- sqrt(2)*(1*pi-pi/2)^{-1}*rt(1, df = 1)*sapply(grid, FUN= function(x) sin((1-1/2)*pi*x)  )
+    for(j in 2:50){
+      # x2[i, ] <- x2[i, ] +(j*pi-pi/2)^{-1}*rnorm(1, 0, 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  )
+      # x2[i,] <- 2*x2[i, ]
+      x2[i, ] <- x2[i, ] + (j*pi-pi/2)^{-1}*rt(1, df = 1)*sqrt(2)*sapply(grid, FUN= function(x) sin((j-1/2)*pi*x)  ) # For leverage contamination
+    }
+  }
+  
+  l2 <- x2%*%f2/p
+  
+  inv.logit <- function(x) 1/(1+exp(-x))
+  y1 <- rbinom(floor(0.9*n), size = 1, prob = inv.logit(l1))
+  y2 <- rbinom((n-floor(0.9*n)), size = 1, prob = inv.logit(l2))
+  y2 <- 1-y2
+  x <- rbind(x1, x2)
+  y <- c(y1, y2)
+  matplot(t(x), type = "l", lwd = 3, col = "gray")
+  
+  fit <- dpd.f(x = x, y = y, norder = 4, m = 2)
+  fit0 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 1e-06)
+  fit05 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 0.5)
+  fit1 <- dpd.ffa(x = x, y = y, norder = 4, m = 2, alpha = 1)
+  msq[k] <- mean((f1-fit$est)^2)
+  alpha.sel[k] <- fit$alpha
+  ic[k] <- fit$ic
+  
+  msq0[k] <- mean((f1-fit0$est)^2)
+  msq05[k] <- mean((f1-fit05$est)^2)
+  msq1[k] <- mean((f1-fit1$est)^2)
+}
+
+hist(alpha.sel)
+# hist(ic)
+mean(msq, na.rm = TRUE)
+median(msq, na.rm = TRUE) ; median(msq0, na.rm = TRUE) ; median(msq05, na.rm = TRUE) ; median(msq1, na.rm = TRUE)
+median(alpha.sel, na.rm = TRUE)
+
+plot(grid, f1, type = "l", lwd = 3)
+lines(grid, fit$est, type = "l", lwd = 3, col = "blue")
+lines(grid, fit0$est)
+
---
+++ fglm.R
@@ -0,0 +1,356 @@
+###################################################################################################################################
+############## Robust and Adaptive Functional Linear Regression (Kalogridis (2023)) ###############################################
+require(fda)
+require(MASS)
+require(maxLik)
+
+dpd.f <- function(x, y, m = 2, nbasis = NULL,  norder = 4, toler = 1e-08, maxiter = 1000, nsteps = 20, 
+                  alpha.cand = c(seq(1e-06, 5, len = 20), 2.5)){
+  
+  # Main function
+  # x is a matrix contained the values of the discretized predictors
+  # y is the response variable
+  # norder is the order of the spline basis, by default a cubic spline basis
+  # m is the order of the penalty, by default the penalty is placed on the integrated squared second derivative
+  # toler is the tolerance level of the Fisher-scoring algorithm
+  # maxiter is the naximum number of allowed iterations of the algorithm
+  # alpha.cand is a vector of candidate values for the tuning parameter (kappa in the notation of the paper)
+  # a pilot estimator corresponding to kappa = 1 is used, as discussed in the paper
+  # nsteps denotes the number of allowed iterations for the selection of the tuning parameter
+  
+  x <- as.matrix(x)
+  n <- length(y)
+  y <- as.vector(y)
+  if(is.null(nbasis)){
+    nbasis = floor(min(n/4, 30))
+  } else{
+    nbasis = nbasis
+  }
+  b.sp <- create.bspline.basis(c(0, 1), nbasis = nbasis, norder = norder)
+  b.sp.e <- eval.basis(seq(1/dim(x)[2], 1-1/dim(x)[2], len = dim(x)[2]), b.sp)
+  x.p.ni <- x%*%b.sp.e/dim(x)[2]
+  x.p <- cbind(rep(1, n), x.p.ni)
+  
+  p.m <- bsplinepen(b.sp, Lfdobj = m )
+  p.m <- rbind( rep(0, nbasis+1 ), cbind(rep(0, nbasis), p.m) )
+  
+  inv.logit <- function(x) 1/(1+exp(-x))
+  beta.in <- rep(0, dim(x.p)[2])
+  
+  obj.f <- function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    t1 <- (1-inv.logit(x.p%*%beta))^{1+alpha} + inv.logit(x.p%*%beta)^{1+alpha}
+    t2 <- (1+1/alpha)*dbinom(y, size = 1, prob = inv.logit(x.p%*%beta))^{alpha}
+    contr <- t1 - t2
+    obj <- as.numeric(mean(contr) + lambda*t(beta)%*%(p.m%*%beta))
+    return(-obj)
+  }
+  
+  gradient <- function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    gr1 <- (0-inv.logit(x.p%*%beta))*(1-inv.logit(x.p%*%beta))^{1+alpha}+ (1-inv.logit(x.p%*%beta))*(inv.logit(x.p%*%beta))^{1+alpha}
+    gr2 <-  (y-inv.logit(x.p%*%beta))*dbinom(y, size = 1 , prob = inv.logit( x.p%*%beta ))^{alpha}
+    gr.t <- gr1 - gr2
+    gr <- (1+alpha)*diag(c(gr.t))%*%x.p
+    gradient <- as.vector(colSums(gr)/n + 2*lambda*(p.m%*%beta))
+    return(-gradient)
+  }
+  
+  hessian <-  function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    wt <- inv.logit(x.p%*%beta)^2*(1-inv.logit(x.p%*%beta))^{1+alpha} + (1-inv.logit(x.p%*%beta ) )^2*(inv.logit(x.p%*%beta))^{1+alpha}
+    wt <- (1+alpha)*wt
+    hs <- scale(t(x.p), center = FALSE, scale = c(1/wt))%*%x.p/n + 2*lambda*p.m
+    return(-hs)
+  }
+  
+  newraph <- function(lambda, beta.in, maxit = maxiter, tol = toler, alpha){
+    mnr <- maxNR(fn = obj.f, grad = gradient, hess = hessian, start = beta.in, pen =  lambda, tun =  alpha,
+                 control = list(tol = tol, reltol = tol, gradtol = tol, iterlim = maxit))
+    beta = mnr$estimate
+    gr1 <- (0-inv.logit(x.p%*%beta))*(1-inv.logit(x.p%*%beta))^{1+alpha}+ (1-inv.logit(x.p%*%beta))*(inv.logit(x.p%*%beta))^{1+alpha}
+    gr2 <-  (y-inv.logit(x.p%*%beta))*dbinom(y, size = 1 , prob = inv.logit( x.p%*%beta ))^{alpha}
+    gr.t <- gr1 - gr2
+    gr <- c((1+alpha)*gr.t)
+    hess.f <- -mnr$hessian
+    Q.m <- hess.f - 2*lambda*p.m
+    # Q.m <- -t(x.p)%*%diag(c(gr*(y-inv.logit(x.p%*%beta))))%*%x.p/n
+    hat.tr <- sum(diag(  solve(hess.f, Q.m, tol = 1e-22) ))/n
+    # hat.tr <- sum(diag(  solve(hess.f)%*% Q.m ) )/n
+    # hat.tr <- sum(diag (ginv(hess.f)%*%Q.m))/n
+    return(list(beta = beta, hat.tr = hat.tr, gr = gr, hess.f = hess.f))
+  }
+  
+  obj.f.np <- function(beta, alpha){
+    t1 <- (1-inv.logit(x.p%*%beta))^{1+alpha} + inv.logit(x.p%*%beta)^{1+alpha}
+    t2 <- (1+1/alpha)*dbinom(y, size = 1, prob = inv.logit(x.p%*%beta))^{alpha}
+    contr <- t1 - t2
+    obj <- as.numeric(mean(contr))
+    return(obj)
+  }
+  
+  Pen.cr <- function(lambda, beta.in, alpha){
+    nr <- newraph(lambda, beta.in = beta.in, maxit = maxiter, alpha = alpha)
+    beta <- nr$beta
+    hat.tr <- nr$hat.tr
+    IC <- 2*(obj.f.np(beta, alpha)) + 2*hat.tr
+    return(IC)
+  }
+  
+  # beta.in <- rnorm(length(beta.in))
+  lambda.cand <- c(1e-12, 6e-12, 1e-11, 6e-11, 1e-10, 6e-10, 1e-09, 5e-09, 9e-09,  1e-08, 5e-08, 9e-08, 1e-07, 5e-07, 9e-07, 1e-06, 5e-06, 9e-06, 1e-05, 5e-05, 9e-05, 1e-04, 5e-04, 9e-04, 
+                   1e-03, 5e-03, 9e-03, 1e-02, 5e-02, 9e-02, 1e-01, 5e-01, 9e-01, 5)
+  lambda.e.in <- rep(0, length(lambda.cand))
+  for(k in 1:length(lambda.e.in)){
+    lambda.e.in[k] <-  try(Pen.cr(lambda.cand[k], beta.in = beta.in, alpha = 2.5),silent = TRUE)
+  }
+  
+  # lambda.e.in <-  try(vapply(lambda.cand, FUN = Pen.cr, beta.in = beta.in, alpha = 2.5, FUN.VALUE = numeric(1)), silent = TRUE)
+  lambda.opt.in <- lambda.cand[which.min(lambda.e.in)]
+  # lambda.opt.in
+  beta.opt.in <- newraph(lambda.opt.in, beta.in = beta.in, maxit = maxiter, alpha = 2.5)$beta
+  B.m <-  bsplinepen(b.sp, Lfdobj = 0)
+
+  comp.alpha <- function(alpha){
+    # lambda.e <-  vapply(lambda.cand, FUN = Pen.cr, beta.in = beta.opt.in, alpha = alpha, FUN.VALUE = numeric(1))
+    # lambda.opt <- lambda.cand[which.min(lambda.e)]
+    lambda.e <- rep(0, length(lambda.cand))
+    for(k in 1:length(lambda.e)){
+      lambda.e[k] <-  try(Pen.cr(lambda.cand[k], beta.in = beta.in, alpha = alpha),silent = TRUE)
+    }
+    lambda.opt <- lambda.cand[which.min(lambda.e)]
+    opt. <- newraph(lambda.opt, beta.in = beta.opt.in, maxit = maxiter, alpha = alpha)
+    beta.opt  <- opt.$beta
+    gr <- opt.$gr
+    hessian.m <- -opt.$hess.f
+    K.m <- scale(t(x.p), center = FALSE, scale = c(1/gr^2))%*%x.p/(n^2) #+ 4*lambda.opt^2*p.m%*%beta.opt%*%t(beta.opt)%*%p.m 
+    msq1 <- sum( diag( B.m%*%solve(hessian.m, K.m%*%solve(hessian.m, tol = 1e-22), tol = 1e-22)[2:(nbasis+1), 2:(nbasis+1)]   ) )
+    # msq1 <- sum(diag ( B.m%*%(ginv(hessian.m)%*%K.m%*%ginv(hessian.m))[2:(nbasis+1), 2:(nbasis+1)]   ))
+    return(list(msq1 = msq1,  beta.opt = beta.opt, lambda.opt = lambda.opt))
+  }
+  
+  compts <-  vapply(alpha.cand, FUN = comp.alpha, FUN.VALUE = rep(list(1), 3) )
+  ic <- 0
+  istop <- 0
+  alpha.in <- 2.5
+  while(ic <= nsteps & istop == 0){
+    ic = ic + 1 
+    msqs <- rep(NA, length(alpha.cand))
+    for(j in 1:length(alpha.cand)){
+      msqs[j] <- compts[[1, j]] + t(compts[[2, j]][-1]-compts[[2, which(alpha.cand == alpha.in)]][-1])%*%B.m%*%(compts[[2, j]][-1]-compts[[2, which(alpha.cand == alpha.in)]][-1])
+    }
+    alpha.up <- alpha.cand[which.min(msqs)]
+    check <- abs(alpha.up-alpha.in)
+    if(check< 1e-06){istop = 1}
+    alpha.in <- alpha.up
+  }
+  alpha.opt <- alpha.up
+  beta.opt.f <- compts[[2, which(alpha.cand == alpha.opt)]]
+  lambda.opt.f <- compts[[3, which(alpha.cand == alpha.up)]]
+  
+  est <-  b.sp.e%*%beta.opt.f[-1]
+  fitted <- inv.logit(beta.opt.f[1]+x%*%est/dim(x)[2])
+  resids <- as.vector(y - inv.logit(fitted))
+  p.resids <- as.vector(resids/sqrt(inv.logit(resids)*(1-inv.logit(resids))))
+  
+  ibeta <- function(x,a,b){ pbeta(x,a,b)*beta(a,b)}
+  ansch.r <- function(y, mu){
+    ansch.r <- (ibeta(y, 2/3, 2/3)-ibeta(mu, 2/3, 2/3))/(mu*(1-mu))^{1/6}
+    return(ansch.r)
+  }
+  a.resids <- ansch.r(y, fitted)
+  
+  return(list(est = est, a.resids = a.resids, alpha = alpha.opt, ic = ic, lambda = lambda.opt.f))
+}
+###########################################################################################################################
+###########################################################################################################################
+
+dpd.ffa <- function(x, y, m = 2, toler = 1e-08, maxiter = 1000, nbasis = 30, norder = 4,  alpha = 1e-04){
+
+  x <- as.matrix(x)
+  n <- length(y)
+  y <- as.vector(y)
+  b.sp <- create.bspline.basis(c(0, 1), nbasis = nbasis, norder = norder)
+  b.sp.e <- eval.basis(seq(1/dim(x)[2], 1-1/dim(x)[2], len = dim(x)[2]), b.sp)
+  x.p.ni <- x%*%b.sp.e/dim(x)[2]
+  x.p <- cbind(rep(1, n), x.p.ni)
+  
+  p.m <- bsplinepen(b.sp, Lfdobj = m )
+  p.m <- rbind( rep(0, nbasis+1 ), cbind(rep(0, nbasis), p.m) )
+  
+  inv.logit <- function(x) 1/(1+exp(-x))
+  beta.il <- rep(0, dim(x.p)[2])
+  
+  obj.f <- function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    t1 <- (1-inv.logit(x.p%*%beta))^{1+alpha} + inv.logit(x.p%*%beta)^{1+alpha}
+    t2 <- (1+1/alpha)*dbinom(y, size = 1, prob = inv.logit(x.p%*%beta))^{alpha}
+    contr <- t1 - t2
+    obj <- as.numeric(mean(contr) + lambda*t(beta)%*%(p.m%*%beta))
+    return(-obj)
+  }
+  
+  gradient <- function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    gr1 <- (0-inv.logit(x.p%*%beta))*(1-inv.logit(x.p%*%beta))^{1+alpha}+ (1-inv.logit(x.p%*%beta))*(inv.logit(x.p%*%beta))^{1+alpha}
+    gr2 <-  (y-inv.logit(x.p%*%beta))*dbinom(y, size = 1 , prob = inv.logit( x.p%*%beta ))^{alpha}
+    gr.t <- gr1 - gr2
+    gr <- (1+alpha)*diag(c(gr.t))%*%x.p
+    gradient <- as.vector(colSums(gr)/n + 2*lambda*(p.m%*%beta))
+    return(-gradient)
+  }
+  
+  hessian <-  function(beta, pen, tun){
+    lambda = pen
+    alpha = tun
+    wt <- inv.logit(x.p%*%beta)^2*(1-inv.logit(x.p%*%beta))^{1+alpha} + (1-inv.logit(x.p%*%beta ) )^2*(inv.logit(x.p%*%beta))^{1+alpha}
+    wt <- (1+alpha)*wt
+    hs <- scale(t(x.p), center = FALSE, scale = c(1/wt))%*%x.p/n + 2*lambda*p.m
+    return(-hs)
+  }
+  
+  newraph <- function(lambda, beta.in, maxit = maxiter, tol = toler, alpha){
+    mnr <- maxNR(fn = obj.f, grad = gradient, hess = hessian, start = beta.in, pen =  lambda, tun =  alpha,
+                 control = list(tol = tol, reltol = tol, gradtol = tol, iterlim = maxit))
+    beta = mnr$estimate
+    gr1 <- (0-inv.logit(x.p%*%beta))*(1-inv.logit(x.p%*%beta))^{1+alpha}+ (1-inv.logit(x.p%*%beta))*(inv.logit(x.p%*%beta))^{1+alpha}
+    gr2 <-  (y-inv.logit(x.p%*%beta))*dbinom(y, size = 1 , prob = inv.logit( x.p%*%beta ))^{alpha}
+    gr.t <- gr1 - gr2
+    gr <- c((1+alpha)*gr.t)
+    hess.f <- -mnr$hessian
+    Q.m <- hess.f - 2*lambda*p.m
+    # Q.m <- -t(x.p)%*%diag(c(gr*(y-inv.logit(x.p%*%beta))))%*%x.p/n
+    hat.tr <- sum(diag(  solve(hess.f, Q.m, tol = 1e-20) ))/n
+    # hat.tr <- sum(diag (ginv(hess.f)%*%Q.m))/n
+    return(list(beta = beta, hat.tr = hat.tr, gr = gr, hess.f = hess.f))
+  }
+  
+  obj.f.np <- function(beta, alpha){
+    t1 <- (1-inv.logit(x.p%*%beta))^{1+alpha} + inv.logit(x.p%*%beta)^{1+alpha}
+    t2 <- (1+1/alpha)*dbinom(y, size = 1, prob = inv.logit(x.p%*%beta))^{alpha}
+    contr <- t1 - t2
+    obj <- as.numeric(mean(contr))
+    return(obj)
+  }
+  
+  Pen.cr <- function(lambda, beta.in, alpha){
+    nr <- newraph(lambda, beta.in = beta.in, maxit = maxiter, alpha = alpha)
+    beta <- nr$beta
+    hat.tr <- nr$hat.tr
+    IC <- 2*(obj.f.np(beta, alpha)) + 2*hat.tr
+    return(IC)
+  }
+  
+  lambda.cand <- c(1e-12, 6e-12, 1e-11, 6e-11, 1e-10, 6e-10, 1e-09, 5e-09, 9e-09,  1e-08, 5e-08, 9e-08, 1e-07, 5e-07, 9e-07, 1e-06, 5e-06, 9e-06, 1e-05, 5e-05, 9e-05, 1e-04, 5e-04, 9e-04, 
+                   1e-03, 5e-03, 9e-03, 1e-02, 5e-02, 9e-02, 1e-01, 5e-01, 9e-01, 5)
+  lambda.e <-  vapply(lambda.cand, FUN = Pen.cr, beta.in = beta.il, alpha = alpha, FUN.VALUE = numeric(1))
+  lambda.opt.f <- lambda.cand[which.min(lambda.e)]
+  
+  beta.opt.f <- newraph(lambda.opt.f, beta.in = beta.il, maxit = maxiter, alpha = alpha)$beta
+  
+  est <-  b.sp.e%*%beta.opt.f[-1]
+  fitted <- inv.logit(beta.opt.f[1]+x%*%est/dim(x)[2])
+  resids <- as.vector(y - inv.logit(fitted))
+  p.resids <- as.vector(resids/sqrt(inv.logit(resids)*(1-inv.logit(resids))))
+  
+  ibeta <- function(x,a,b){ pbeta(x,a,b)*beta(a,b)}
+  ansch.r <- function(y, mu){
+    ansch.r <- (ibeta(y, 2/3, 2/3)-ibeta(mu, 2/3, 2/3))/(mu*(1-mu))^{1/6}
+    return(ansch.r)
+  }
+  a.resids <- ansch.r(y, fitted)
+  
+  return(list(est = est, a.resids = a.resids, lambda = lambda.opt.f))
+}
+
+###############################################################################################################################
+###############################################################################################################################
+
+p.ml <- function(x, y, m = 2, toler = 1e-08, maxiter = 1000, nbasis = 30, norder = 4){
+  
+  x <- as.matrix(x)
+  n <- length(y)
+  y <- as.vector(y)
+  b.sp <- create.bspline.basis(c(0, 1), nbasis = nbasis, norder = norder)
+  b.sp.e <- eval.basis(seq(1/dim(x)[2], 1-1/dim(x)[2], len = dim(x)[2]), b.sp)
+  x.p.ni <- x%*%b.sp.e/dim(x)[2]
+  x.p <- cbind(rep(1, n), x.p.ni)
+  
+  p.m <- bsplinepen(b.sp, Lfdobj = m )
+  p.m <- rbind( rep(0, nbasis+1 ), cbind(rep(0, nbasis), p.m) )
+  
+  inv.logit <- function(x) 1/(1+exp(-x))
+  beta.il <- rep(0, dim(x.p)[2])
+  
+  obj.f <- function(beta, pen){
+    lambda = pen
+    obj <- mean(log(dbinom(y, size = 1, prob = inv.logit(x.p%*%beta)))) - lambda*t(beta)%*%(p.m%*%beta)
+    return(obj)
+  }
+  
+  gradient <- function(beta, pen){
+    lambda = pen
+    gr.t <- y-inv.logit(x.p%*%beta)
+    gr <- diag(c(gr.t))%*%x.p
+    gradient <- as.vector(colSums(gr)/n - 2*lambda*(p.m%*%beta))
+    return(gradient)
+  }
+  
+  hessian <-  function(beta, pen){
+    lambda = pen
+    wt <- -inv.logit(x.p%*%beta)*(1-inv.logit(x.p%*%beta))
+    hs <- scale(t(x.p), center = FALSE, scale = c(1/wt))%*%x.p/n - 2*lambda*p.m
+    return(hs)
+  }
+  
+  newraph <- function(lambda, beta.in, maxit = maxiter, tol = toler){
+    mnr <- maxNR(fn = obj.f, grad = gradient, hess = hessian, start = beta.in, pen =  lambda,
+                 control = list(tol = tol, reltol = tol, gradtol = tol, iterlim = maxit))
+    beta = mnr$estimate
+    gr.t <- c(y-inv.logit(x.p%*%beta))
+    hess.f <- mnr$hessian
+    Q.m <- (hess.f + 2*lambda*p.m)
+    # Q.m <- -t(x.p)%*%diag(c(gr.t*(y-inv.logit(x.p%*%beta))))%*%x.p/n
+    hat.tr <- sum(diag(  solve(hess.f, Q.m, tol = 1e-22) ))/n
+    return(list(beta = beta, hat.tr = hat.tr, gr = gr.t, hess.f = hess.f))
+  }
+  
+  obj.f.np <- function(beta){
+    obj <- mean(log(dbinom(y, size = 1, prob = inv.logit(x.p%*%beta))))
+    return(obj)
+  }
+  
+  Pen.cr <- function(lambda, beta.in){
+    nr <- newraph(lambda, beta.in = beta.in, maxit = maxiter)
+    beta <- nr$beta
+    hat.tr <- nr$hat.tr
+    IC <- 2*(obj.f.np(beta)) - 2*hat.tr
+    return(IC)
+  }
+  
+  lambda.cand <- c(1e-12, 6e-12, 1e-11, 6e-11, 1e-10, 6e-10, 1e-09, 5e-09, 9e-09,  1e-08, 5e-08, 9e-08, 1e-07, 5e-07, 9e-07, 1e-06, 5e-06, 9e-06, 1e-05, 5e-05, 9e-05, 1e-04, 5e-04, 9e-04, 
+                   1e-03, 5e-03, 9e-03, 1e-02, 5e-02, 9e-02, 1e-01, 5e-01, 9e-01, 5)
+  lambda.e <-  vapply(lambda.cand, FUN = Pen.cr, beta.in = beta.il, FUN.VALUE = numeric(1))
+  lambda.opt.f <- lambda.cand[which.max(lambda.e)]
+  
+  beta.opt.f <- newraph(lambda.opt.f, beta.in = beta.il, maxit = maxiter)$beta
+  
+  est <-  b.sp.e%*%beta.opt.f[-1]
+  fitted <- inv.logit(beta.opt.f[1]+x%*%est/dim(x)[2])
+  resids <- as.vector(y - inv.logit(fitted))
+  p.resids <- as.vector(resids/sqrt(inv.logit(resids)*(1-inv.logit(resids))))
+  
+  ibeta <- function(x,a,b){ pbeta(x,a,b)*beta(a,b)}
+  ansch.r <- function(y, mu){
+    ansch.r <- (ibeta(y, 2/3, 2/3)-ibeta(mu, 2/3, 2/3))/(mu*(1-mu))^{1/6}
+    return(ansch.r)
+  }
+  a.resids <- ansch.r(y, fitted)
+  
+  return(list(est = est, a.resids = a.resids, lambda = lambda.opt.f))
+}
