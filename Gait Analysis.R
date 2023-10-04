95b02da6fbb08ae30b22ce8f88f4c1c5ab39e553
ioanniskalogridis <77626768+ioanniskalogridis@users.noreply.github.com>
2023-10-04 13:44:50 +0200

    Update Gait Analysis.R

--- Gait Analysis.R
+++ Gait Analysis.R
@@ -18,8 +18,31 @@ fit.ml <- dpd.f(x1, y1, tuning = 1e-04)
 fit.robust$alpha 
 # Notice that a large tuning parameter is selected
 # Plot the estimators
-plot(fit.robust$est, type = "l", lwd = 3, col = "blue",  cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "", ylim = c(-100, 100)) ; grid()
+plot(fit.robust$est, type = "l", lwd = 3, col = "blue",  cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "") ; grid()
 lines(fit.ml$est, type = "l", col = "red", lwd = 3, lty = 2)
+
+u.tr <- function(x, alpha){
+  n <- length(x)
+  m <- round(alpha*n)
+  x.s <- sort(x)
+  u.tr <- mean(x.s[1:(n-m)])
+  return(u.tr)
+}
+
+fitted.r <- predict.dpd(x1, fit.robust)
+sum(fitted.r$fitted.prob)
+u.tr(fitted.r$fitted.prob[y1==1&abs(fit.robust$a.resids) <= 2], alpha = 0.00001) # correctly classified as "normal", normal correctly classified as normal
+u.tr(fitted.r$fitted.prob[y1==0&abs(fit.robust$a.resids) <= 2], alpha = 0.1) # abnormal incorrectly classified as "normal"
+
+
+fitted.ml <- predict.dpd(x1, fit.ml)
+sum(fitted.ml$fitted.prob)
+u.tr(fitted.ml$fitted.prob[y1==1&abs(fit.robust$a.resids) <= 2], alpha = 0.1) # correctly classified as "normal", normal correctly classified as normal
+u.tr(fitted.ml$fitted.prob[y1==0&abs(fit.robust$a.resids) <= 2], alpha = 0.1) # abnormal incorrectly classified as "normal"
+
+which(y1==1)
+mean(fitted.r$fitted.prob[which(y1==1)])
+
 
 # Check Anscombe residuals
 hist(fit.robust$a.resids, cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "", main = "") ; grid()
@@ -41,12 +64,47 @@ fit.ml.wo <- dpd.f(x2, y2, tuning = 1e-04)
 fit.robust.wo$alpha
 # Notice the low value of the tuning that is selected after the outliers have been removed
 
-plot(fit.robust.wo$est, type = "l", lwd = 3, col = "blue",  cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "",
-     ylim = c(-100, 100)) ; grid()
+plot(fit.robust.wo$est, type = "l", lwd = 3, col = "blue",  cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "") ; grid()
 lines(fit.ml.wo$est, type = "l", col = "red", lwd = 3, lty = 2)
 
 # Check residuals again
 hist(fit.robust.wo$a.resids, cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "")
 hist(fit.ml.wo$a.resids, cex = 2.5, cex.axis = 3, cex.lab = 2.5, xlab = "", ylab = "")
 # Identical results after the removal of the outliers
+
+#################################5-fold CV ##################################################################
+
+u.tr <- function(x, alpha){
+  n <- length(x)
+  m <- round(alpha*n)
+  x.s <- sort(x)
+  u.tr <- mean(x.s[1:(n-m)])
+  return(u.tr)
+}
+
+cv.function <- function(x, y, nfolds, int){
+  x <- as.matrix(x)
+  remainder = dim(x)[1]%%nfolds
+  splits <- split(1:dim(x)[1], sample( rep(1:nfolds, times = c(floor(dim(x)[1]/nfolds+remainder), rep( floor(dim(x)[1]/nfolds), nfolds-1 ))  ) ) )
+  rmspe.r <- rep(NA, nfolds)
+  rmpse.ls <- rep(NA, nfolds)
+  
+  for(j in 1:nfolds){
+    x.test <- x[splits[[j]], ]
+    x.train <- x[-splits[[j]], ]
+    y.test <- y[splits[[j]]]
+    y.train <- y[-splits[[j]]]
+    fit.r <- dpd.f(x = x.train, y = y.train)
+    fit.ml <- dpd.f(x = x.train, y = y.train, tuning = 1e-04)
+    
+    x.test <- as.matrix(x.test)
+    pred.values.r <- predict.dpd( x.test, fit.r)
+    pred.values.ml <- predict.dpd( x.test, fit.ml)
+    rmspe.r[j] <- sqrt(u.tr( (y.test-pred.values.r$fitted.prob)^2, 0.1 ))
+    rmpse.ls[j] <- sqrt(u.tr( (y.test-pred.values.ml$fitted.prob)^2,0.1 ))
+  }
+  rmspe.r <- mean(rmspe.r)
+  rmpse.ls <- mean(rmpse.ls)
+  return(list(rmspe.r, rmpse.ls))
+}
 
